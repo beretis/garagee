77a6efeb86fba4737fa6668d4694ccd09fb5ee71
Jozef Matus <jojo@matus.sk>
2018-02-02 11:12:24 +0100

    pods

---
+++ garagee/generated/CreatePartVCVM.swift
@@ -0,0 +1,22 @@
+// Generated using Sourcery 0.10.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import Foundation
+import RxCocoa
+import RxSwift
+import RxViewModel
+
+class CreatePartVCVM: BaseViewModel, RxDefaultErrorHandlable {
+
+    public var defaultHandler: RxErrorHandler
+
+	init(coordinator: ExploreCoordinatorType, defaultErrorHandler: RxErrorHandler) {
+		self.defaultHandler = defaultErrorHandler
+        super.init()
+		self.coordinator = coordinator
+		self.setupRx()
+    }
+
+    private func setupRx() {
+    }
+}
